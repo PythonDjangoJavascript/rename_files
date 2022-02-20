23a253236821ed21179feb4a2c0242e230057788
nuruddinsayeed <nuruddinsayeed@gmail.com>
2022-02-20 16:06:27 +0600

    Rename Script completed

---
+++ rename.py
@@ -0,0 +1,53 @@
+import os
+import shutil
+from openpyxl import load_workbook
+from pathlib import Path
+
+file_extension = ".jpg"
+
+wb = load_workbook('rename_files.xlsx')
+ws = wb.active
+
+vals = ws.values
+
+to_dir = os.getcwd() + "/for_rename/"
+
+rename_data = {}
+for val in vals:
+    if val[0] is not None and val[1] is not None:
+        rename_data[str(val[0]).strip() + file_extension] = to_dir +  str(val[1]).strip() + file_extension
+        
+# print(rename_data)
+        
+
+print(to_dir)
+a = 0
+for x in os.listdir():
+    if x.endswith(file_extension):
+        # Prints only text file present in My Folder
+        if rename_data.get(x) is not None:
+            # shutil.copy(x, to_dir)
+            shutil.move(x, rename_data.get(x), copy_function=shutil.copy2)
+            a += 1
+        else:
+            print(x)
+            
+print(f'total {a}')
+    
+# for x in os.listdir():
+#     if x.endswith(file_extension):
+#         # Prints only text file present in My Folder
+#         if rename_data.get(x) is not None:
+#             os.rename(x, rename_data.get(x))
+#             print(f'renamed {x}  new: -> {rename_data.get(x)}')
+
+# a = 0
+# for x in os.listdir():
+#     if x.endswith(file_extension):
+#         # Prints only text file present in My Folder
+#         if rename_data.get(x) is not None:
+#             # os.rename(x, rename_data.get(x))
+#             a += 1
+#             print(f'renamed {x}  new: -> {rename_data.get(x)}')
+            
+# print(f'total {a}')
